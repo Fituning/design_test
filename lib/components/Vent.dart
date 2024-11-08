0eb74e317fbec4490fa812dbb534466165624b7f
Sroÿce <carstar@live.fr>
2024-11-08 10:42:53 +0100

    fix vent update event

--- lib/components/Vent.dart
+++ lib/components/Vent.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:api_car_repository/api_car_repository.dart';
@@ -11,75 +12,18 @@ import 'ElevatedButton.dart';
 import 'NotifPastille.dart';
 
 const int minVentLevel = 0;
-const int maxVentLevel = 3;
-
-
-
-class VentController2 extends StatefulWidget {
-  const VentController2({
-    super.key,
-  });
-
-  @override
-  State<VentController2> createState() => _VentControllerState2();
-}
-
-class _VentControllerState2 extends State<VentController2> {
-  var ventLevel = 1;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        CircularElevatedButton(icon: const FaIcon(FontAwesomeIcons.minus), onPressed: (){
-          if(ventLevel >0){
-            ventLevel -= 1;
-          }
-          print(ventLevel);
-        }),
-        const SizedBox(width: 32,),
-        Column(
-          children: [
-            SvgPicture.asset(
-              "assets/images/AirTurbine.svg",
-              height: 33,
-            ),
-            const SizedBox(height: 6,),
-            Row(
-              children: [
-                NotificationBadge(radius: 8,activeColor: (ventLevel >= 1) ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.primaryContainer,),
-                const SizedBox(width: 8,),
-                NotificationBadge(radius: 8,activeColor: (ventLevel >= 2) ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.primaryContainer,),
-                const SizedBox(width: 8,),
-                NotificationBadge(radius: 8,activeColor: (ventLevel >= 3) ? Theme.of(context).colorScheme.secondary : Theme.of(context).colorScheme.primaryContainer,),
-              ],
-            )
-          ],
-        ),
-        const SizedBox(width: 32,),
-        CircularElevatedButton(icon: const FaIcon(FontAwesomeIcons.plus), onPressed: (){
-          if(ventLevel <3){
-            ventLevel += 1;
-          }
-        }),
-      ],
-    );
-  }
-}
-
+int maxVentLevel = VentilationLevelEnum.values.length - 1;
 
 class VentController extends StatefulWidget {
-  const VentController({
-    super.key,
-  });
+  const VentController({super.key});
 
   @override
   State<VentController> createState() => _VentControllerState();
 }
 
 class _VentControllerState extends State<VentController> {
-  // int ventLevel = 1;
+  VentilationLevelEnum ventLevel = VentilationLevelEnum.level2;
+  Timer? _debounce; // Déclaration du Timer pour la logique de délai
 
   @override
   Widget build(BuildContext context) {
@@ -101,40 +45,54 @@ class _VentControllerState extends State<VentController> {
     );
   }
 
-  Widget _display(BuildContext context, Car car){
+  //Pour mettre un delais avant d'envoyer la requete
+  void _onVentilationLevelChange(int newLevel) {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 600), () {
+      context.read<CarBloc>().add(UpdateAirConditioning(
+        ventilationLevel: VentilationLevelEnumExtension.fromInt(newLevel),
+      ));
+    });
+  }
+
+  Widget _display(BuildContext context, Car car) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         CircularElevatedButton(
-            icon: const FaIcon(FontAwesomeIcons.minus),
-            onPressed: () {
-              // setState(() {
-              //   ventLevel = max(minVentLevel, ventLevel - 1);
-              // });
-              setState(() {
-                car.airConditioning.ventilationLevel = VentilationLevelEnumExtension.fromInt(max(minVentLevel, car.airConditioning.ventilationLevel.index - 1));
-              });
-              context.read<CarBloc>().add(UpdateAirConditioning(ventilationLevel: car.airConditioning.ventilationLevel));
-            }),
-        const SizedBox(width: 32,),
+          icon: const FaIcon(FontAwesomeIcons.minus),
+          onPressed: () {
+            setState(() {
+              // Mettre à jour l'état local
+              car.airConditioning.ventilationLevel = VentilationLevelEnumExtension.fromInt(
+                max(minVentLevel, car.airConditioning.ventilationLevel.index - 1),
+              );
+            });
+            // Déclencher la requête avec un délai
+            _onVentilationLevelChange(car.airConditioning.ventilationLevel.index);
+          },
+        ),
+        const SizedBox(width: 32),
         VentLevelIndicator(ventLevel: car.airConditioning.ventilationLevel.index),
-        const SizedBox(width: 32,),
+        const SizedBox(width: 32),
         CircularElevatedButton(
-            icon: const FaIcon(FontAwesomeIcons.plus),
-            onPressed: () {
-              setState(() {
-                car.airConditioning.ventilationLevel = VentilationLevelEnumExtension.fromInt(min(maxVentLevel, car.airConditioning.ventilationLevel.index + 1));
-              });
-              context.read<CarBloc>().add(UpdateAirConditioning(ventilationLevel: car.airConditioning.ventilationLevel));
-            }
-            ),
+          icon: const FaIcon(FontAwesomeIcons.plus),
+          onPressed: () {
+            setState(() {
+              // Mettre à jour l'état local
+              car.airConditioning.ventilationLevel = VentilationLevelEnumExtension.fromInt(
+                min(maxVentLevel, car.airConditioning.ventilationLevel.index + 1),
+              );
+            });
+            // Déclencher la requête avec un délai
+            _onVentilationLevelChange(car.airConditioning.ventilationLevel.index);
+          },
+        ),
       ],
     );
   }
 }
-
-
 
 class VentLevelIndicator extends StatelessWidget {
   const VentLevelIndicator({super.key, required this.ventLevel});
@@ -143,23 +101,37 @@ class VentLevelIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(ventLevel);
     return Column(
       children: [
         SvgPicture.asset(
           "assets/images/AirTurbine.svg",
           height: 33,
         ),
-        const SizedBox(height: 6,),
+        const SizedBox(height: 6),
         Row(
           children: [
-            NotificationBadge(radius: 8, active: (ventLevel >= 1), activeColor:  Theme.of(context).colorScheme.secondary, inactiveColor:  Theme.of(context).colorScheme.primaryContainer,),
-            const SizedBox(width: 8,),
-            NotificationBadge(radius: 8, active: (ventLevel >= 2), activeColor:  Theme.of(context).colorScheme.secondary, inactiveColor:  Theme.of(context).colorScheme.primaryContainer,),
-            const SizedBox(width: 8,),
-            NotificationBadge(radius: 8, active: (ventLevel >= 3), activeColor:  Theme.of(context).colorScheme.secondary, inactiveColor:  Theme.of(context).colorScheme.primaryContainer,),
+            NotificationBadge(
+              radius: 8,
+              active: (ventLevel >= 1),
+              activeColor: Theme.of(context).colorScheme.secondary,
+              inactiveColor: Theme.of(context).colorScheme.primaryContainer,
+            ),
+            const SizedBox(width: 8),
+            NotificationBadge(
+              radius: 8,
+              active: (ventLevel >= 2),
+              activeColor: Theme.of(context).colorScheme.secondary,
+              inactiveColor: Theme.of(context).colorScheme.primaryContainer,
+            ),
+            const SizedBox(width: 8),
+            NotificationBadge(
+              radius: 8,
+              active: (ventLevel >= 3),
+              activeColor: Theme.of(context).colorScheme.secondary,
+              inactiveColor: Theme.of(context).colorScheme.primaryContainer,
+            ),
           ],
-        )
+        ),
       ],
     );
   }
